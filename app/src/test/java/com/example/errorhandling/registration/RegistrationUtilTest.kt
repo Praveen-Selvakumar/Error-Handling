c91699a894192d75086a6a6bc470179d32f36428
Praveen-Selvakumar <praveen.ad.selvakumar@gmail.com>
2025-05-30 08:05:28 +0530

    Push Passed TestCases.

--- app/src/test/java/com/example/errorhandling/registration/RegistrationUtilTest.kt
+++ app/src/test/java/com/example/errorhandling/registration/RegistrationUtilTest.kt
@@ -7,20 +7,20 @@ class RegistrationUtilTest {
 
     @Test
     fun `name is less that 3 return false`() {
-        val result = RegistrationUtil().isDetailsValid(name = "sr")
+        val result = RegistrationUtil().isDetailsValid(name = "Praveen", password = "123", confirmPassword = "123")
         assert(result)
     }
 
 
     @Test
     fun `password is not equal return false`(){
-        val  result = RegistrationUtil().isDetailsValid(password = "123", confirmPassword = "321")
+        val  result = RegistrationUtil().isDetailsValid(name = "Praveen Selvakumar",password = "123", confirmPassword = "123")
         assert(result)
     }
 
     @Test
     fun `pass testcase if all are valid`(){
-        val result = RegistrationUtil().isDetailsValid(name = "Praveen", password =  "uumai007", confirmPassword = "uumai007")
+        val result = RegistrationUtil().isDetailsValid(name = "Praveen Selvakumar", password =  "uumai007", confirmPassword = "uumai007")
         assert(result)
     }
 
